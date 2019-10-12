371c7a77c21a783f3f95eb299d3506aa05a856c3
leroilaura <laura.leroi@ifremer.fr>
2019-10-12 17:52:40 +0200

    add dependency

--- R/run_app.R
+++ R/run_app.R
@@ -3,8 +3,8 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 #'
 #' @export
 library(shinydashboard)
+source("import/ui_import.R")
 source("semantics/semanticsUI.R")
-#set working directory from actual file path
 
 #run shiny app
 runApp(".")
