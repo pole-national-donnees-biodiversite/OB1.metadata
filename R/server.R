cc1fa907e4e6262aad891a1b3acce678c28fb7a1
leroilaura <laura.leroi@ifremer.fr>
2019-10-12 18:24:55 +0200

    callModule import

--- R/server.R
+++ R/server.R
@@ -3,5 +3,6 @@ app_server <- function(input, output,session) {
   session$onSessionEnded(stopApp)
   
   # List the first level callModules here
+  callModule(server_import(),"import_id",NULL)
   callModule(semantics, "semantics", NULL)
 }
