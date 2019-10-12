d4d9c2cae62856a36f562837fd399911e7cd9d74
leroilaura <laura.leroi@ifremer.fr>
2019-10-12 16:44:06 +0200

    add first dashboard + rename app.R -> ui.R

---
+++ R/ui.R
@@ -0,0 +1,40 @@
+#' @import shiny
+app_ui <- function() {
+  #tagList(
+  # Leave this function for adding external resources
+  # golem_add_external_resources(),
+  # List the first level UI elements here 
+  fluidPage(
+    dashboardPage(
+      title = "OB1.metadata",
+      dashboardHeader(
+        title = "OB1.metadata"),
+      dashboardSidebar(
+        menuItem("Welcome", tabName = "welcome", 
+                 icon = icon("home"))
+      ),
+      dashboardBody(
+        tabItems(
+          tabItem(tabName = "welcome", fluidPage())
+        )
+      )
+    )
+  )
+}
+
+#' @import shiny
+#golem_add_external_resources <- function(){
+
+#addResourcePath(
+#  'www', system.file('app/www', package = 'OB1.metadata')
+#  )
+
+# tags$head(
+#  golem::activate_js(),
+# golem::favicon()
+# Add here all the external resources
+# If you have a custom.css in the inst/app/www
+# Or for example, you can add shinyalert::useShinyalert() here
+#tags$link(rel="stylesheet", type="text/css", href="www/custom.css")
+#  )
+#}
